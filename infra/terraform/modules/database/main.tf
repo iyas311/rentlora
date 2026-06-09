3bd544944308c7d8a4bd2aee8da31e64fed50317
iyas311 <iyas2458@gmail.com>
2026-06-09 13:26:44 +0530

    feat: add terraform module for RDS PostgreSQL instance and SSM parameter storage

--- infra/terraform/modules/database/main.tf
+++ infra/terraform/modules/database/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "main" {
 resource "aws_db_instance" "postgres" {
   identifier             = "${var.project_name}-db"
   engine                 = "postgres"
-  engine_version         = "15.3"
+  engine_version         = "15.7"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
   storage_type           = "gp2"
