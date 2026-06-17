2598e3ab0d42b94d13ea91731697282d9b02f6c3
iyas311 <iyas2458@gmail.com>
2026-06-17 15:34:38 +0530

    feat: add RDS endpoint output to test environment terraform configuration

--- infra/terraform/environments/test/outputs.tf
+++ infra/terraform/environments/test/outputs.tf
@@ -1,12 +1,3 @@
-output "cloudfront_domain_name" {
-  description = "The CloudFront Domain Name for the application"
-  value       = module.cdn.cloudfront_domain_name
-}
-
-output "external_alb_dns" {
-  description = "The DNS name of the External Application Load Balancer"
-  value       = module.load_balancing.ext_alb_dns_name
-}
 
 output "rds_endpoint" {
   description = "The RDS PostgreSQL endpoint"
